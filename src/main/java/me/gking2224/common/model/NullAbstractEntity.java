0ca1eb6e2cfcf0b1930b134e851a49923640d4a3
Graham King <gking2224@gmail.com>
2016-11-12 13:08:23 +0000

    json ignore null entity bean

--- src/main/java/me/gking2224/common/model/NullAbstractEntity.java
+++ src/main/java/me/gking2224/common/model/NullAbstractEntity.java
@@ -2,6 +2,8 @@ package me.gking2224.common.model;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public abstract class NullAbstractEntity<K extends Serializable> extends AbstractEntity<K, NullEntityBean>
 implements Serializable {
 
@@ -11,6 +13,7 @@ implements Serializable {
     private static final long serialVersionUID = -9178571050108131441L;
 
     @Override
+    @JsonIgnore
     public final NullEntityBean getBean() {
         return NullEntityBean.INSTANCE;
     }
