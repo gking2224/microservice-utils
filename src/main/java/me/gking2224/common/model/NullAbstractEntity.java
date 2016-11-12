53abe99775478efa14dc422fd263e10f3c9bba4e
Graham King <gking2224@gmail.com>
2016-11-12 11:05:41 +0000

    serial id

--- src/main/java/me/gking2224/common/model/NullAbstractEntity.java
+++ src/main/java/me/gking2224/common/model/NullAbstractEntity.java
@@ -2,7 +2,13 @@ package me.gking2224.common.model;
 
 import java.io.Serializable;
 
-public abstract class NullAbstractEntity<K extends Serializable> extends AbstractEntity<K, NullEntityBean> {
+public abstract class NullAbstractEntity<K extends Serializable> extends AbstractEntity<K, NullEntityBean>
+implements Serializable {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -9178571050108131441L;
 
     @Override
     public final NullEntityBean getBean() {
