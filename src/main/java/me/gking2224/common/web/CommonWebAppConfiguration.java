76040718b4f621892a343983bae687dd9b0c8f63
Graham King <gking2224@gmail.com>
2016-11-15 16:15:23 +0000

    cors

--- src/main/java/me/gking2224/common/web/CommonWebAppConfiguration.java
+++ src/main/java/me/gking2224/common/web/CommonWebAppConfiguration.java
@@ -12,7 +12,9 @@ import org.springframework.util.AntPathMatcher;
 import org.springframework.util.PathMatcher;
 import org.springframework.web.servlet.config.annotation.CorsRegistration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 import org.springframework.web.util.UrlPathHelper;
 
@@ -22,10 +24,7 @@ import me.gking2224.common.client.EnvironmentProperties;
 @ComponentScan("me.gking2224.common.web")
 @Profile("web")
 @EnvironmentProperties(value="props:/webapp.properties", name="common-webapp", prefix="web")
-public class CommonWebAppConfiguration extends WebMvcConfigurationSupport {
-
-    @Autowired
-    private WebConfigurationOptions options;
+public class CommonWebAppConfiguration {
     
 //    @SuppressWarnings("unused")
 //    private ApplicationContext applicationContext;
@@ -60,12 +59,16 @@ public class CommonWebAppConfiguration extends WebMvcConfigurationSupport {
         AntPathMatcher matcher = new AntPathMatcher();
         return matcher;
     }
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurerAdapter() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                CorsRegistration mapping = registry.addMapping("/**");
+//                mapping.allowedMethods(options.getAllowedCorsMethods());
+//                mapping.allowedOrigins(options.getAllowedCorsOrigins());
+//            }
+//        };
+//    }
 
-    @Override
-    protected void addCorsMappings(CorsRegistry registry) {
-        CorsRegistration mapping = registry.addMapping("/**");
-        mapping.allowedMethods(options.getAllowedCorsMethods());
-        mapping.allowedOrigins(options.getAllowedCorsOrigins());
-        
-    }
 }
